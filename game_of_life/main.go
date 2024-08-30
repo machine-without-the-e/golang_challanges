7a8488deba6cbc62c79dff66c311e941018e6068
Aaron Machin <machinaaron1@gmail.com>
2024-08-30 20:55:33 +1200

    wip:

--- game_of_life/main.go
+++ game_of_life/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
   "fmt"
+  "math/rand"
 )
 
 type board struct {
-  canvas [10][10]cell
+  canvas [50][50]cell
 }
 
 func (b *board) setup() {
-  b.canvas[1][1].alive = 1;
+
+  for index_row, row := range b.canvas {
+    for index_column := range row {
+      b.canvas[index_row][index_column].position = position{
+        row: index_row,
+        column: index_column,
+      }
+
+      b.canvas[index_row][index_column].alive = rand.Intn(2)
+
+    }
+  }
 }
 
 func (b *board) draw() {
@@ -26,7 +38,7 @@ func (b *board) draw() {
 func (b *board) calculateNextGen() {
   for i, row := range b.canvas {
     for j, cell := range row {
-      neighboursAlive := cell.neighboursAlive()
+      neighboursAlive := b.neighboursAlive(cell.position)
       if(cell.alive == 0) {
         if(neighboursAlive == 3) {
           b.canvas[i][j].alive = 1
@@ -49,12 +61,75 @@ func (b *board) calculateNextGen() {
   }
 }
 
-type cell struct {
-  alive int
+type position struct {
+  row int
+  column int
 }
 
-func (c *cell) neighboursAlive() int {
-  return 0;
+type cell struct {
+  alive int
+  position position
+}
+
+func (b *board) neighboursAlive(p position) int {
+  positions_to_check := []position{
+    {
+      row: -1,
+      column: -1,
+    },
+    {
+      row: -1,
+      column: 0,
+    },
+    {
+      row: -1,
+      column: 1,
+    },
+    {
+      row: 0,
+      column: -1,
+    },
+    {
+      row: 0,
+      column: 1,
+    },
+    {
+      row: 1,
+      column: -1,
+    },
+    {
+      row: 1,
+      column: 0,
+    },
+    {
+      row: 1,
+      column: 1,
+    },
+  }
+
+  neighbours := 0
+
+  for _, pos_to_check := range positions_to_check {
+    newPosition := position {
+      row: p.row + pos_to_check.row,
+      column: p.column + pos_to_check.column,
+    }
+
+    if(newPosition.row >= len(b.canvas) || newPosition.row < 0) {
+      continue
+    }
+
+    if(newPosition.column >= len(b.canvas[newPosition.row]) || newPosition.column < 0) {
+      continue
+    }
+
+
+    if(b.canvas[p.row + pos_to_check.row][p.column + pos_to_check.column].alive == 1) {
+      neighbours++
+    }
+  }
+
+  return neighbours
 }
 
 func main() {
